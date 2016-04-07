d2ec7c0cdd0c2e1108fc1de609030fc3b2dda5d6
Chris Dodd <chris@barefootnetworks.com>
2016-04-07 11:25:34 -0700

    Reenable v1to12 and bmv2 tests on tofino sample files

--- test-v12-sample.sh
+++ test-v12-sample.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-dir=$1
+srcdir=$1
+execdir=$(dirname $0)
 shift
-$dir/backends/v12test/run-v12-sample.py $dir -a "-D__TARGET_TOFINO__ -I $dir/backends/tofino/v1_samples/p4_lib" $*
+$srcdir/backends/v12test/run-v12-sample.py $srcdir -a "-D__TARGET_TOFINO__ -I $execdir/v1_samples/p4_lib" $*
