b907f8df3fe4f0689fcdcdd775d1217b5cd5e301
Chris Dodd <chris@barefootnetworks.com>
2016-06-12 20:05:56 -0700

    adjust Makefile for testdir restructuring

--- test-v12-sample.sh
+++ test-v12-sample.sh
@@ -3,4 +3,4 @@
 srcdir=$1
 execdir=$(dirname $0)
 shift
-$srcdir/backends/v12test/run-v12-sample.py $srcdir -a "-D__TARGET_TOFINO__ -I $execdir/v1_samples/p4_lib" $*
+$srcdir/backends/p4test/run-p4-sample.py $srcdir -a "-D__TARGET_TOFINO__ -I $execdir/v1_samples/p4_lib" $*
