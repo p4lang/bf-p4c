0e049a47a85ddb2e28c78422904866c814613ccd
Chris Dodd <chris@barefootnetworks.com>
2016-07-29 11:36:51 -0700

    toJSON changes to output directly to stream -- implement toJSON in .def files

--- mau/resource.h
+++ mau/resource.h
@@ -19,7 +19,7 @@ struct TableResourceAlloc {
         match_ixbar.clear();
         gateway_ixbar.clear();
         memuse.clear(); }
-    cstring toJSON(JSONGenerator &) const { return ""; }
+    void toJSON(JSONGenerator &json) const { json << "null"; }
 };
 
 #endif /* _TOFINO_MAU_RESOURCE_H_ */
