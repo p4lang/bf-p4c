354b6380a8c206b0467fc7df0ee533b7846b7cec
Chris Dodd <chris@barefootnetworks.com>
2017-01-16 16:37:01 -0800

    move std p4 includes to p4include and p4_14include

--- test-v12-sample.sh
+++ test-v12-sample.sh
@@ -3,4 +3,4 @@
 srcdir=$1
 execdir=$(dirname $0)
 shift
-$srcdir/backends/p4test/run-p4-sample.py $srcdir -a "-D__TARGET_TOFINO__ -I $execdir/v1_samples/p4_lib" $*
+$srcdir/backends/p4test/run-p4-sample.py $srcdir -a "-D__TARGET_TOFINO__ -I $execdir/p4_14include" $*
