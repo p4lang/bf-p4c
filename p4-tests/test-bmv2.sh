856397a242f0838a16255a523c2aa9ad335f7d0c
Chris Dodd <chris@barefootnetworks.com>
2017-01-24 10:09:26 -0800

    remove p4_lib, adjust XFAILs

--- p4-tests/test-bmv2.sh
+++ p4-tests/test-bmv2.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 srcdir=$1
-execdir=$(dirname $0)
 shift
-$srcdir/backends/bmv2/run-bmv2-test.py $srcdir -a "-D__TARGET_TOFINO__ -I $execdir/p4_14/p4_lib" $*
+$srcdir/backends/bmv2/run-bmv2-test.py $srcdir -a "-D__TARGET_TOFINO__ -I $srcdir/extensions/tofino/p4_14include" $*
