741a202cdea5f7a26ecb388af2f101ad5e7d9c8e
Chris Dodd <chris@barefootnetworks.com>
2017-01-27 15:49:51 -0800

    remove explicit -I paths for p4includes (no longer needed)

--- p4-tests/test-bmv2.sh
+++ p4-tests/test-bmv2.sh
@@ -2,4 +2,4 @@
 
 srcdir=$1
 shift
-$srcdir/backends/bmv2/run-bmv2-test.py $srcdir -a "-D__TARGET_TOFINO__ -I $srcdir/extensions/tofino/p4_14include" $*
+$srcdir/backends/bmv2/run-bmv2-test.py $srcdir -a -D__TARGET_TOFINO__ $*
--- p4-tests/test-v12-sample.sh
+++ p4-tests/test-v12-sample.sh
@@ -2,4 +2,4 @@
 
 srcdir=$1
 shift
-$srcdir/backends/p4test/run-p4-sample.py $srcdir -a "-D__TARGET_TOFINO__ -I $srcdir/extensions/tofino/p4_14include" $*
+$srcdir/backends/p4test/run-p4-sample.py $srcdir -a -D__TARGET_TOFINO__ $*
