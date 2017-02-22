cceb03aa3749d774023a1e3b4d5e32abf8613d7d
Chris Dodd <chris@barefootnetworks.com>
2017-02-22 12:28:15 -0800

    Fix warning

--- mau/table_dependency_graph.h
+++ mau/table_dependency_graph.h
@@ -151,7 +151,7 @@ class FindDependencyGraph : public MauInspector, ControlFlowVisitor {
 
     bool preorder(const IR::MAU::TableSeq *) override;
     bool preorder(const IR::MAU::Table *) override;
-    void end_apply() { finalize_dependence_graph(); }
+    void end_apply() override { finalize_dependence_graph(); }
 
     void flow_merge(Visitor &v) override;
 
