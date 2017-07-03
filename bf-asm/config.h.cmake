f49cbab5534a01c04445536fd78618ef69af249f
Calin Cascaval <cascaval@barefootnetworks.com>
2017-07-02 19:15:30 -0700

    cmake support for the assembler

---
+++ bf-asm/config.h.cmake
@@ -0,0 +1,8 @@
+/* Define to 1 if you have the execinfo.h header */
+#cmakedefine HAVE_EXECINFO_H 1
+
+/* Define to 1 if you have the LIBGC library. */
+#cmakedefine HAVE_LIBGC 1
+
+/* Define to 1 if you have the ucontext.h header */
+#cmakedefine HAVE_UCONTEXT_H 1
