05f96e595e3f5e443e612a0fc573053b9b061335
Seth Fowler <seth@blackhail.net>
2017-07-10 22:44:28 -0700

    Only round in ComputeShifts at the end of a ParserMatch

--- bf-p4c/parde/compute_shifts.h
+++ bf-p4c/parde/compute_shifts.h
@@ -4,17 +4,30 @@
 #include "parde_visitor.h"
 
 class ComputeShifts : public PardeModifier {
-    struct CountExtracts : public Inspector {
-        int &count;
-        void postorder(const IR::Primitive *prim) override {
-            if (prim->name == "extract")
-                count += (prim->operands[0]->type->width_bits() + 7) / 8U; }
-        explicit CountExtracts(int &c) : count(c) {}
-    };
-    void postorder(IR::Tofino::ParserMatch *match) override {
-        if (match->shift < 0) {
-            match->shift = 0;
-            match->stmts.apply(CountExtracts(match->shift)); } }
+    void postorder(IR::Tofino::ParserMatch* match) override {
+        if (match->shift >= 0) return;  // We already computed the shift.
+
+        unsigned bits = 0;
+        forAllMatching<IR::Primitive>(&match->stmts, [&](const IR::Primitive* p) {
+            if (p->name == "extract")
+                bits += p->operands[0]->type->width_bits();
+            return false;
+        });
+
+        // On Tofino we can't parse or deparse headers correctly if they aren't
+        // byte-aligned. Practically speaking, that means that the extractions
+        // in a ParserMatch must be byte-aligned as well; we're careful not to
+        // violate this requirement in compiler-generated code, and user
+        // generated code always extracts entire headers. If this is violated,
+        // the resulting program is unlikely to work correctly, though we'll
+        // round up and soldier on in the interest of producing some sort of
+        // output.
+        if (bits % 8 != 0)
+            ::warning("Parser extractions are not byte-aligned in state %1%",
+                      findContext<IR::Tofino::ParserState>());
+
+        match->shift = (bits + 7) / 8U;
+    }
 };
 
 #endif /* _TOFINO_PARDE_COMPUTE_SHIFTS_H_ */
