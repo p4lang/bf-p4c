5e3252cc098940e48a3bc929f53c114cb8b45ed1
Antonin Bas <antonin@barefootnetworks.com>
2017-08-14 14:08:14 -0700

    Do not duplicate stringify in PTF tests

--- p4-tests/p4_14/easy_exact.ptf/test.py
+++ p4-tests/p4_14/easy_exact.ptf/test.py
@@ -2,20 +2,11 @@ import ptf
 from ptf import config
 import ptf.testutils as testutils
 
-#import pdb
 from p4 import p4runtime_pb2
 from p4.tmp import p4config_pb2
 from p4.config import p4info_pb2
 
-from base_test import P4RuntimeTest
-
-# Takes a value and the desired length in hexadecimal. Outputs it stringified,
-# with \x separating the bytes.
-def stringify(val, length):
-    s = format(val, '0%dx' % length)
-    pairs = [s[i:i+2] for i in xrange(0, len(s), 2)]
-
-    return "".join(pairs).decode("hex")
+from base_test import P4RuntimeTest, stringify
 
 # Creates a dumbPacket corresponding to the header h_t.
 def dumbPacket(f1=0xabcd, f2=0xef, f3=0xaa):
@@ -23,18 +14,14 @@ def dumbPacket(f1=0xabcd, f2=0xef, f3=0xaa):
     assert 0 <= f2 <= 0xff
     assert 0 <= f3 <= 0xff
 
-    s = stringify(f1, 4) + stringify(f2, 2) + stringify(f3, 2)
+    s = stringify(f1, 2) + stringify(f2, 1) + stringify(f3, 1)
 
     # Add fake payload.
-    s += '000000000000000'
+    s += '0' * 15
     return s
 
 class SimpleTest(P4RuntimeTest):
-
     def runTest(self):
-        print "Running test"
-
-#        pdb.set_trace()
         # Sending a packet when no entry is installed. Should miss.
         pkt = dumbPacket(0xdead, 0x00, 0x00)
         testutils.send_packet(self, 1, str(pkt))
@@ -51,20 +38,24 @@ class SimpleTest(P4RuntimeTest):
         # Add a single entry: matches on the 8 bit length prefix of "0xdead",
         # and sends the packet to port number 3, while setting the h.f2 field
         # to 0x42.
+        f1 = 0xdead
+        val = 0x42
+        eg_port = 3
         self.set_match_key(
             table_entry, "t",
-            [self.Exact("h.f1", "\xde\xad")])
-        self.set_action_entry(table_entry, "do", [("val", "\x42"), ("port", "\x00\x03")])
+            [self.Exact("h.f1", stringify(f1, 2))])
+        self.set_action_entry(
+            table_entry, "do",
+            [("val", stringify(val, 1)), ("port", stringify(eg_port, 2))])
 
-        # Send the gRPC request.
         rep = self.stub.Write(req)
 
         # Send a matching packet, with the exact value matching.
         # Expect packet on port 3, with a f2 field of 0x42.
-        pkt = dumbPacket(f1=0xdead, f2=0x00, f3=0x00)
+        pkt = dumbPacket(f1=f1, f2=0x00, f3=0x00)
         testutils.send_packet(self, 1, str(pkt))
-        exp_pkt = dumbPacket(f1=0xdead, f2=0x42, f3=0x00)
-        testutils.verify_packet(self, exp_pkt, 3)
+        exp_pkt = dumbPacket(f1=f1, f2=val, f3=0x00)
+        testutils.verify_packet(self, exp_pkt, eg_port)
 
         # Send a non matching packet.
         pkt = dumbPacket(f1=0x00aa, f2=0x00, f3=0x00)
