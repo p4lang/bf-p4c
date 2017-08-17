43e5655ef2a3216ea418092dc0dfe266db404351
Seth Fowler <seth@blackhail.net>
2017-08-17 02:27:40 -0700

    Fix TrivialAlloc GTest errors and make the GTest code more concise

--- bf-p4c/test/gtest/trivial_alloc.cpp
+++ bf-p4c/test/gtest/trivial_alloc.cpp
@@ -192,141 +192,125 @@ TYPED_TEST(TofinoPHVTrivialAllocators, AutomaticAllocation) {
         SCOPED_TRACE(gress);
         auto prefix = cstring::to_cstring(gress) + "::";
 
-        // Check that the provided container index is valid for this thread.
-        auto indexOK = [=](unsigned index) {
-            return gress == INGRESS ? (index < 16 || index >= 32)
-                                    : (index >= 16);
+        using SliceId = std::pair<cstring, unsigned>;
+        std::map<SliceId, PHV::Container> containers;
+
+        // Check that the provided container can be allocated to this thread.
+        auto okForCurrentThread = [=](PHV::Container container) {
+            return gress == INGRESS ? !PHV::Container::egressOnly()[container.id()]
+                                    : !PHV::Container::ingressOnly()[container.id()];
+        };
+
+        // A helper that checks that the given field's allocation maps the given
+        // field bit ranges to the provided container bit ranges.
+        auto checkMapping =
+          [&](cstring fieldName, const std::vector<bitrange>& fieldBits,
+                                 const std::vector<bitrange>& containerBits) {
+            SCOPED_TRACE(fieldName.c_str());
+
+            // Sanity check that the field has an allocation and that it has the
+            // right size.
+            auto field = phv.field(prefix + fieldName);
+            ASSERT_TRUE(field != nullptr);
+            auto& fieldAlloc = this->alloc(field);
+            ASSERT_EQ(fieldAlloc.size(), fieldBits.size());
+            ASSERT_EQ(containerBits.size(), fieldBits.size());
+
+            // Check each slice in the allocation.
+            for (unsigned i = 0; i < fieldBits.size(); ++i) {
+                auto& slice = fieldAlloc[i];
+                EXPECT_EQ(fieldBits[i].lo, slice.field_bits().lo);
+                EXPECT_EQ(fieldBits[i].hi, slice.field_bits().hi);
+                EXPECT_EQ(containerBits[i].lo, slice.container_bits().lo);
+                EXPECT_EQ(containerBits[i].hi, slice.container_bits().hi);
+                EXPECT_PRED1(okForCurrentThread, slice.container);
+
+                auto sliceId = std::make_pair(fieldName, i);
+                EXPECT_TRUE(containers.find(sliceId) == containers.end())
+                            << "Checked slice more than once?";
+                containers[sliceId] = slice.container;
+            }
         };
 
         // header H1 { bit<8> field; }
-        auto h1_field = phv.field(prefix + "h1.field");
-        ASSERT_TRUE(h1_field != nullptr);
-        auto h1_field_alloc = this->alloc(h1_field);
-        ASSERT_EQ(1u, h1_field_alloc.size());
-        EXPECT_EQ(0, h1_field_alloc[0].container_bit);
-        EXPECT_EQ(0, h1_field_alloc[0].field_bit);
-        EXPECT_EQ(8, h1_field_alloc[0].width);
-        auto h1_field_container = h1_field_alloc[0].container;
-        EXPECT_EQ(PHV::Container::Kind::B, h1_field_container.kind());
-        EXPECT_PRED1(indexOK, h1_field_container.index());
-        EXPECT_TRUE(uniqueContainers.insert(h1_field_container).second);
+        checkMapping("h1.field", { bitrange(0, 7) }, { bitrange(0, 7) });
+        auto h1Container = containers[SliceId("h1.field", 0)];
+        EXPECT_EQ(PHV::Container::Kind::B, h1Container.kind());
+        EXPECT_TRUE(uniqueContainers.insert(h1Container).second);
 
         // header H2 { bit<1> field1; bit<6> field2; bit<9> field3; }
-        auto h2_field1 = phv.field(prefix + "h2.field1");
-        ASSERT_TRUE(h2_field1 != nullptr);
-        auto h2_field1_alloc = this->alloc(h2_field1);
-        ASSERT_EQ(1u, h2_field1_alloc.size());
-        EXPECT_EQ(15, h2_field1_alloc[0].container_bit);
-        EXPECT_EQ(0, h2_field1_alloc[0].field_bit);
-        EXPECT_EQ(1, h2_field1_alloc[0].width);
-        auto h2_field2 = phv.field(prefix + "h2.field2");
-        ASSERT_TRUE(h2_field2 != nullptr);
-        auto h2_field2_alloc = this->alloc(h2_field2);
-        ASSERT_EQ(1u, h2_field2_alloc.size());
-        EXPECT_EQ(9, h2_field2_alloc[0].container_bit);
-        EXPECT_EQ(0, h2_field2_alloc[0].field_bit);
-        EXPECT_EQ(6, h2_field2_alloc[0].width);
-        auto h2_field3 = phv.field(prefix + "h2.field3");
-        ASSERT_TRUE(h2_field3 != nullptr);
-        auto h2_field3_alloc = this->alloc(h2_field3);
-        ASSERT_EQ(1u, h2_field3_alloc.size());
-        EXPECT_EQ(0, h2_field3_alloc[0].container_bit);
-        EXPECT_EQ(0, h2_field3_alloc[0].field_bit);
-        EXPECT_EQ(9, h2_field3_alloc[0].width);
-        auto h2_field_container = h2_field1_alloc[0].container;
-        EXPECT_EQ(h2_field_container, h2_field2_alloc[0].container);
-        EXPECT_EQ(h2_field_container, h2_field3_alloc[0].container);
-        EXPECT_EQ(PHV::Container::Kind::H, h2_field_container.kind());
-        EXPECT_PRED1(indexOK, h2_field_container.index());
-        EXPECT_TRUE(uniqueContainers.insert(h2_field_container).second);
+        checkMapping("h2.field1", { bitrange(0, 0) }, { bitrange(15, 15) });
+        checkMapping("h2.field2", { bitrange(0, 5) }, { bitrange(9, 14) });
+        checkMapping("h2.field3", { bitrange(0, 8) }, { bitrange(0, 8) });
+        auto h2Container = containers[SliceId("h2.field1", 0)];
+        EXPECT_EQ(h2Container, containers[SliceId("h2.field2", 0)]);
+        EXPECT_EQ(h2Container, containers[SliceId("h2.field3", 0)]);
+        EXPECT_EQ(PHV::Container::Kind::H, h2Container.kind());
+        EXPECT_TRUE(uniqueContainers.insert(h2Container).second);
 
         // header H3 { bit<72> field; }
-        auto h3_field = phv.field(prefix + "h3.field");
-        ASSERT_TRUE(h3_field != nullptr);
-        auto h3_field_alloc = this->alloc(h3_field);
-        ASSERT_EQ(3u, h3_field_alloc.size());
-        EXPECT_EQ(0, h3_field_alloc[0].container_bit);
-        EXPECT_EQ(40, h3_field_alloc[0].field_bit);
-        EXPECT_EQ(32, h3_field_alloc[0].width);
-        auto h3_field_container_0 = h3_field_alloc[0].container;
-        EXPECT_EQ(PHV::Container::Kind::W, h3_field_container_0.kind());
-        EXPECT_PRED1(indexOK, h3_field_container_0.index());
-        EXPECT_TRUE(uniqueContainers.insert(h3_field_container_0).second);
-        EXPECT_EQ(0, h3_field_alloc[1].container_bit);
-        EXPECT_EQ(8, h3_field_alloc[1].field_bit);
-        EXPECT_EQ(32, h3_field_alloc[1].width);
-        auto h3_field_container_1 = h3_field_alloc[1].container;
-        EXPECT_EQ(PHV::Container::Kind::W, h3_field_container_1.kind());
-        EXPECT_PRED1(indexOK, h3_field_container_1.index());
-        EXPECT_TRUE(uniqueContainers.insert(h3_field_container_1).second);
-        EXPECT_EQ(0, h3_field_alloc[2].container_bit);
-        EXPECT_EQ(0, h3_field_alloc[2].field_bit);
-        EXPECT_EQ(8, h3_field_alloc[2].width);
-        auto h3_field_container_2 = h3_field_alloc[2].container;
-        EXPECT_EQ(PHV::Container::Kind::B, h3_field_container_2.kind());
-        EXPECT_PRED1(indexOK, h3_field_container_2.index());
-        EXPECT_TRUE(uniqueContainers.insert(h3_field_container_2).second);
+        checkMapping("h3.field",
+                     { bitrange(40, 71), bitrange(8, 39), bitrange(0, 7) },
+                     { bitrange(0, 31), bitrange(0, 31), bitrange(0, 7) });
+        auto h3Slice0Container = containers[SliceId("h3.field", 0)];
+        EXPECT_EQ(PHV::Container::Kind::W, h3Slice0Container.kind());
+        EXPECT_TRUE(uniqueContainers.insert(h3Slice0Container).second);
+        auto h3Slice1Container = containers[SliceId("h3.field", 1)];
+        EXPECT_EQ(PHV::Container::Kind::W, h3Slice1Container.kind());
+        EXPECT_TRUE(uniqueContainers.insert(h3Slice1Container).second);
+        auto h3Slice2Container = containers[SliceId("h3.field", 2)];
+        EXPECT_EQ(PHV::Container::Kind::B, h3Slice2Container.kind());
+        EXPECT_TRUE(uniqueContainers.insert(h3Slice2Container).second);
 
         // header H4 { bit<16> field; }
         // (This field isn't used in the MAU, so we check that it gets assigned
         // to a tagalong container below.)
-        auto h4_field = phv.field(prefix + "h4.field");
-        ASSERT_TRUE(h4_field != nullptr);
-        auto h4_field_alloc = this->alloc(h4_field);
-        ASSERT_EQ(1u, h4_field_alloc.size());
-        EXPECT_EQ(0, h4_field_alloc[0].container_bit);
-        EXPECT_EQ(0, h4_field_alloc[0].field_bit);
-        EXPECT_EQ(16, h4_field_alloc[0].width);
-        auto h4_field_container = h4_field_alloc[0].container;
-        EXPECT_EQ(PHV::Container::Kind::TH, h4_field_container.kind());
-        EXPECT_TRUE(uniqueContainers.insert(h4_field_container).second);
+        checkMapping("h4.field", { bitrange(0, 15) }, { bitrange(0, 15) });
+        auto h4Container = containers[SliceId("h4.field", 0)];
+        EXPECT_EQ(PHV::Container::Kind::TH, h4Container.kind());
+        EXPECT_TRUE(uniqueContainers.insert(h4Container).second);
 
         // header H5 { bit<32> field; }
         // (This field isn't used at all, so we don't expect it to get any
-        // alloc_iation whatsoever.)
+        // allocation whatsoever.)
         EXPECT_TRUE(phv.field(prefix + "h5.field") == nullptr);
         EXPECT_TRUE(phv.field(prefix + "h5.$valid") == nullptr);
 
         // Check the POV bits.
-        auto h1_valid = phv.field(prefix + "h1.$valid");
-        ASSERT_TRUE(h1_valid != nullptr);
-        auto h1_valid_alloc = this->alloc(h1_valid);
-        ASSERT_EQ(1u, h1_valid_alloc.size());
-        EXPECT_EQ(7, h1_valid_alloc[0].container_bit);
-        EXPECT_EQ(0, h1_valid_alloc[0].field_bit);
-        EXPECT_EQ(1, h1_valid_alloc[0].width);
-        auto h2_valid = phv.field(prefix + "h2.$valid");
-        ASSERT_TRUE(h2_valid != nullptr);
-        auto h2_valid_alloc = this->alloc(h2_valid);
-        ASSERT_EQ(1u, h2_valid_alloc.size());
-        EXPECT_EQ(6, h2_valid_alloc[0].container_bit);
-        EXPECT_EQ(0, h2_valid_alloc[0].field_bit);
-        EXPECT_EQ(1, h2_valid_alloc[0].width);
-        auto h3_valid = phv.field(prefix + "h3.$valid");
-        ASSERT_TRUE(h3_valid != nullptr);
-        auto h3_valid_alloc = this->alloc(h3_valid);
-        ASSERT_EQ(1u, h3_valid_alloc.size());
-        EXPECT_EQ(5, h3_valid_alloc[0].container_bit);
-        EXPECT_EQ(0, h3_valid_alloc[0].field_bit);
-        EXPECT_EQ(1, h3_valid_alloc[0].width);
-        auto h4_valid = phv.field(prefix + "h4.$valid");
-        ASSERT_TRUE(h4_valid != nullptr);
-        auto h4_valid_alloc = this->alloc(h4_valid);
-        ASSERT_EQ(1u, h4_valid_alloc.size());
-        // XXX(seth): TrivialAlloc and ManualAlloc get different results here
-        // because TrivialAlloc places the final field in a container as far to
-        // the right as it can, even if it introduces a gap.
-        int expected_h4_valid_alloc_container_bit =
-            std::is_same<TypeParam, PHV::TrivialAlloc>::value ? 0 : 4;
-        EXPECT_EQ(expected_h4_valid_alloc_container_bit,
-                  h4_valid_alloc[0].container_bit);
-        EXPECT_EQ(0, h4_valid_alloc[0].field_bit);
-        EXPECT_EQ(1, h4_valid_alloc[0].width);
-        auto h1_valid_container = h1_valid_alloc[0].container;
-        EXPECT_EQ(PHV::Container::Kind::B, h1_field_container.kind());
-        EXPECT_EQ(h1_valid_container, h2_valid_alloc[0].container);
-        EXPECT_EQ(h1_valid_container, h3_valid_alloc[0].container);
-        EXPECT_EQ(h1_valid_container, h4_valid_alloc[0].container);
+        {
+            // A helper that produces the container range for the next POV bit;
+            // helps work around the fact that ingress and egress have different
+            // intrinsic metadata that results in a different set of POV bits.
+            int nextBitIndex = 7;
+            auto nextBit = [&] {
+                auto range = bitrange(nextBitIndex, nextBitIndex);
+                EXPECT_GE(nextBitIndex, 0);
+                nextBitIndex--;
+                return range;
+            };
+
+            if (gress == INGRESS)
+                checkMapping("$always_deparse", { bitrange(0, 0) }, { nextBit() });
+            checkMapping("h1.$valid", { bitrange(0, 0) }, { nextBit() });
+            checkMapping("h2.$valid", { bitrange(0, 0) }, { nextBit() });
+            checkMapping("h3.$valid", { bitrange(0, 0) }, { nextBit() });
+
+            // XXX(seth): TrivialAlloc and ManualAlloc get different results here
+            // because TrivialAlloc places the final field in a container as far to
+            // the right as it can, even if it introduces a gap.
+            nextBitIndex =
+              std::is_same<TypeParam, PHV::TrivialAlloc>::value ? 0 : nextBitIndex;
+            checkMapping("h4.$valid", { bitrange(0, 0) }, { nextBit() });
+        }
+
+        auto povContainer = containers[SliceId("h1.$valid", 0)];
+        EXPECT_EQ(povContainer, containers[SliceId("h2.$valid", 0)]);
+        EXPECT_EQ(povContainer, containers[SliceId("h3.$valid", 0)]);
+        EXPECT_EQ(povContainer, containers[SliceId("h4.$valid", 0)]);
+        if (gress == INGRESS)
+            EXPECT_EQ(povContainer, containers[SliceId("$always_deparse", 0)]);
+        EXPECT_EQ(PHV::Container::Kind::B, povContainer.kind());
+        EXPECT_TRUE(uniqueContainers.insert(povContainer).second);
     }
 }
 
