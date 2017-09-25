6bc889f6bea3eefd481088b88dfaa27dc8114679
Antonin Bas <antonin@barefootnetworks.com>
2017-09-25 09:44:48 -0700

    Set P4C_TOFINO_VERSION to 6.0.0-pre-alpha.2

--- bf-p4c/version.h
+++ bf-p4c/version.h
@@ -5,6 +5,6 @@ Copyright 2013-present Barefoot Networks, Inc.
 #ifndef __TOFINO_VERSION__
 #define __TOFINO_VERSION__
 
-#define P4C_TOFINO_VERSION "6.0.0-pre-alpha"
+#define P4C_TOFINO_VERSION "6.0.0-pre-alpha.2"
 
 #endif // __TOFINO_VERSION__
