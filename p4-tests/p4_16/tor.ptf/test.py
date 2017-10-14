f4e3fdbeab6a40dc67c5d8bcd475648a243c5ed0
Antonin Bas <antonin@barefootnetworks.com>
2017-10-14 13:30:49 -0700

    Add PTF test to check group members distribution
    Added this PTF test to tor.p4, because I thought there might have been
    an issue with a incorrect distribution (all packets going to some member
    when they shouldn't), but the test is passing even though the
    distribution is pretty skewed (which is not abnormal given the
    parameters of the test). I have yet to test in on HW.

--- p4-tests/p4_16/tor.ptf/test.py
+++ p4-tests/p4_16/tor.ptf/test.py
@@ -82,6 +82,121 @@ class FullTestMembersOnly(FullTest):
         # Pick entries from 'tor.ptf/write_lpm_entries_members_only.pb.txt'
         self.runCommonTest("write_lpm_entries_members_only.pb.txt")
 
+class HashDistributionTest(P4RuntimeTest):
+    def add_member(self, mbr_id, eg_port, smac, dmac):
+        print "Creating member", mbr_id
+        eg_port_str = stringify(eg_port, 2)
+        req = p4runtime_pb2.WriteRequest()
+        req.device_id = self.device_id
+        update = req.updates.add()
+        update.type = p4runtime_pb2.Update.INSERT
+        ap_member = update.entity.action_profile_member
+        ap_member.action_profile_id = self.get_ap_id(
+            "l3_fwd.wcmp_action_profile")
+        ap_member.member_id = mbr_id
+        self.set_action(ap_member.action, "l3_fwd.set_nexthop",
+                        [("port", eg_port_str), ("smac", smac), ("dmac", dmac)])
+        rep = self.stub.Write(req)
+        self.requests.append(req)
+
+    def add_group(self, grp_id):
+        print "Creating group", grp_id
+        req = p4runtime_pb2.WriteRequest()
+        req.device_id = self.device_id
+        update = req.updates.add()
+        update.type = p4runtime_pb2.Update.INSERT
+        ap_group = update.entity.action_profile_group
+        ap_group.action_profile_id = self.get_ap_id(
+            "l3_fwd.wcmp_action_profile")
+        ap_group.group_id = grp_id
+        ap_group.max_size = 32
+        rep = self.stub.Write(req)
+        self.requests.append(req)
+
+    def set_group_membership(self, grp_id, mbr_ids = []):
+        print "Setting members for group", grp_id
+        req = p4runtime_pb2.WriteRequest()
+        req.device_id = self.device_id
+        update = req.updates.add()
+        update.type = p4runtime_pb2.Update.MODIFY
+        ap_group = update.entity.action_profile_group
+        ap_group.action_profile_id = self.get_ap_id(
+            "l3_fwd.wcmp_action_profile")
+        ap_group.group_id = grp_id
+        for mbr_id in mbr_ids:
+            member = ap_group.members.add()
+            member.member_id = mbr_id
+        rep = self.stub.Write(req)
+
+    def add_entry_to_group(self, mac, pref, pLen, grp_id):
+        print "Adding match entry to group", grp_id
+        req = p4runtime_pb2.WriteRequest()
+        req.device_id = self.device_id
+        update = req.updates.add()
+        update.type = p4runtime_pb2.Update.INSERT
+        table_entry = update.entity.table_entry
+        table_entry.table_id = self.get_table_id(
+            "l3_fwd.l3_routing_classifier_table")
+        self.set_match_key(
+            table_entry, "l3_fwd.l3_routing_classifier_table",
+            [self.Exact("hdr.ethernet.dst_addr", mac)])
+        self.set_action_entry(table_entry, "NoAction", [])
+        update = req.updates.add()
+        update.type = p4runtime_pb2.Update.INSERT
+        table_entry = update.entity.table_entry
+        table_entry.table_id = self.get_table_id(
+            "l3_fwd.l3_ipv4_override_table")
+        self.set_match_key(
+            table_entry, "l3_fwd.l3_ipv4_override_table",
+            [self.Lpm("hdr.ipv4_base.dst_addr", pref, pLen)])
+        table_entry.action.action_profile_group_id = grp_id
+        rep = self.stub.Write(req)
+        self.requests.append(req)
+
+    def runtest(self):
+        ig_port = self.swports(3)
+        mbr1, port1 = 1, self.swports(1)
+        mbr2, port2 = 2, self.swports(2)
+        smac = "\xee\xcd\x00\x7e\x70\x00"
+        dmac = "\xee\x30\xca\x9d\x1e\x00"
+        self.add_member(mbr1, port1, smac, dmac)
+        self.add_member(mbr2, port2, smac, dmac)
+        grp = 3
+        self.add_group(grp)
+        self.set_group_membership(grp, [mbr1, mbr2])
+        ip_src = "10.0.0.2"
+        ip_dst = "10.0.0.1"
+        ip_dst_str = "\x0a\x00\x00\x01"
+        self.add_entry_to_group(smac, ip_dst_str, 32, grp)
+        npkts = 30
+        counts = [0, 0]
+        tcp_dport = 5001
+        for i in xrange(npkts):
+            tcp_sport = 4001 + i
+            pkt = testutils.simple_tcp_packet(
+                eth_dst = smac, ip_src = ip_src, ip_dst = ip_dst, ip_ttl = 64,
+                tcp_sport = tcp_sport, tcp_dport = tcp_dport)
+            exp_pkt = testutils.simple_tcp_packet(
+                eth_dst = dmac, eth_src = smac,
+                ip_src = ip_src, ip_dst = ip_dst, ip_ttl = 63,
+                tcp_sport = tcp_sport, tcp_dport = tcp_dport)
+            testutils.send_packet(self, ig_port, str(pkt))
+            port_index, _ = testutils.verify_packet_any_port(
+                self, exp_pkt, [port1, port2])
+            counts[port_index] += 1
+        print "Port counts :", counts
+        freqs = [1. * c / npkts for c in counts]
+        print "Port frequencies :", freqs
+        for c in counts:
+            self.assertGreater(c, npkts / 4)
+
+    def runTest(self):
+        self.requests = []
+        try:
+            self.runtest()
+        finally:
+            self.undo_write_requests(self.requests)
+
 class PacketOutTest(P4RuntimeTest):
     def runTest(self):
         # We test the case where the packet is skipping ingress & egress
