6a613e5aba5364bd1fda5e3a5bca45f9df8801ce
Calin Cascaval <cascaval@barefootnetworks.com>
2017-12-23 12:54:17 -0800

    cleanup package installation from source

--- install_os_deps.sh
+++ install_os_deps.sh
@@ -24,80 +24,153 @@ install_python_packages() {
 }
 
 install_linux_packages() {
-    ubuntu_release=$(lsb_release -r | cut -f 2)
-    nprocs=$(cat /proc/cpuinfo | grep processor | wc -l)
-    gcc_ver=""
-    if [[ $ubuntu_release =~ "14.04" ]]; then
-        sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-        gcc_ver="-4.9"
-        # need a way to install boost libraries
-        boost_libs=""
-    else
-        boost_libs="libboost-dev libboost-graph-dev libboost-test-dev libboost-program-options-dev libboost-system-dev libboost-filesystem-dev libboost-iostreams-dev libboost-thread-dev"
-    fi
+    local ubuntu_release=$(lsb_release -r | cut -f 2)
+    local nprocs=$(cat /proc/cpuinfo | grep processor | wc -l)
 
-    apt_packages="g++${gcc_ver} git pkg-config automake cmake curl python2.7 python bison flex ${boost_libs} libtool libcli-dev libedit-dev libeditline-dev libevent-dev libjudy-dev libgc-dev libgmp-dev libjson0 libjson0-dev libmoose-perl libnl-route-3-dev libpcap0.8-dev libssl-dev autopoint doxygen texinfo python-scapy python-yaml python-ipaddr python-pip rpm realpath unzip"
+    # please keep this list in alphabetical order
+    apt_packages="automake autopoint bison curl doxygen flex g++ git \
+                 libcli-dev libedit-dev libeditline-dev libevent-dev \
+                 libgc-dev libgmp-dev libjson0 libjson0-dev libjudy-dev \
+                 libmoose-perl libnl-route-3-dev libpcap0.8-dev libssl-dev \
+                 libtool pkg-config \
+                 python2.7 python python-ipaddr python-pip \
+                 python-scapy python-yaml \
+                 realpath rpm texinfo unzip"
 
     echo "Need sudo privs to install apt packages"
     $SUDO apt-get update || die "Failed to update apt"
-    $SUDO apt-get install -y $apt_packages || die "Failed to install needed packages"
+    $SUDO apt-get install -y $apt_packages || \
+        die "Failed to install needed packages"
     if [[ $ubuntu_release =~ "14.04" ]]; then
+        $SUDO add-apt-repository ppa:ubuntu-toolchain-r/test
+        $SUDO apt-get install -y g++-4.9
         $SUDO update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 10
         $SUDO update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 20
         $SUDO update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 10
         $SUDO update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 20
-
     fi
 
     $SUDO apt-get remove -y python-thrift    # remove this broken package in case it was installed
     install_python_packages
 
-    if [[ $ubuntu_release =~ "14.04" ]]; then
-        # install cmake 3.5.2
-        cmake_ver=3.5.2
-        cd /tmp
-        wget http://www.cmake.org/files/v3.5/cmake-${cmake_ver}.tar.gz && \
-        tar zxvf cmake-${cmake_ver}.tar.gz && \
-        cd cmake-${cmake_ver} && \
-        mkdir build && \
-        cd build && \
-        cmake .. -DCMAKE_BUILD_TYPE=release && \
-        make -j $nprocs
-        cpack -G DEB && \
-        $SUDO apt-get remove -y cmake cmake-data && \
-        $SUDO dpkg -i cmake-${cmake_ver}-Linux-x86_64.deb || \
-        die "failed to install cmake"
-        cd /tmp && rm -rf /tmp/cmake-${cmake_ver}
+    install_cmake "3.5.2"
+    install_boost "1.58.0"
+    install_rapidjson "1.1.0"
+}
 
-        # boost libs
-        sudo apt-get install -y python-dev libbz2-dev
-        boost_ver=1_58_0
+function install_cmake() {
+    # ARG: cmake version
+    cmake_ver=$1
+    cmake_dir=$(echo ${cmake_ver} | cut -f 1-2 -d ".")
+    rc=($SUDO apt-get install -y cmake=${cmake_ver})
+    if [ $? != 0 ]; then
+        local nprocs=$(cat /proc/cpuinfo | grep processor | wc -l)
         cd /tmp
-        wget http://downloads.sourceforge.net/project/boost/boost/1.58.0/boost_${boost_ver}.tar.bz2 && \
-        tar xvjf ./boost_${boost_ver}.tar.bz2 && \
-        cd boost_${boost_ver} && \
-        ./bootstrap.sh --prefix=/usr/local && \
-        ./b2 --build-type=minimal link=shared runtime-link=shared variant=release && \
-        sudo ./b2 install --build-type=minimal link=shared runtime-link=shared variant=release || \
-        die "failed to install boost"
+        wget http://www.cmake.org/files/v${cmake_dir}/cmake-${cmake_ver}.tar.gz && \
+            tar zxvf cmake-${cmake_ver}.tar.gz && \
+            cd cmake-${cmake_ver} && \
+            mkdir build && \
+            cd build && \
+            cmake .. -DCMAKE_BUILD_TYPE=release && \
+            make -j $nprocs && \
+            cpack -G DEB && \
+            $SUDO apt-get remove -y cmake cmake-data && \
+            $SUDO dpkg -i cmake-${cmake_ver}-Linux-x86_64.deb || \
+                die "failed to install cmake"
+        cd /tmp && rm -rf /tmp/cmake-${cmake_ver}
+    fi
+}
+
+function install_boost() {
+    # ARG: boost_version
+    # test for ubuntu version, and check if we have a local package
+    local ubuntu_release=$(lsb_release -r | cut -f 2)
+    if [[ $ubuntu_release =~ "14.04" ]]; then
+        sudo apt-get install -y python-dev libbz2-dev
+        b_ver=$1
+        boost_ver=$(echo $b_ver | tr "." "_")
+        cd /tmp
+        wget http://downloads.sourceforge.net/project/boost/boost/${b_ver}/boost_${boost_ver}.tar.bz2 && \
+            tar xvjf ./boost_${boost_ver}.tar.bz2 && \
+            cd boost_${boost_ver} && \
+            ./bootstrap.sh --prefix=/usr/local && \
+            ./b2 --build-type=minimal link=shared runtime-link=shared variant=release && \
+            $SUDO ./b2 install --build-type=minimal variant=release \
+                  link=shared runtime-link=shared || \
+                die "failed to install boost"
         cd /tmp && rm -rf boost_${boost_ver}
+    else
+        boost_libs="libboost-dev libboost-graph-dev libboost-test-dev libboost-program-options-dev libboost-system-dev libboost-filesystem-dev libboost-iostreams-dev libboost-thread-dev"
+        $SUDO apt-get install -y ${boost_libs}
     fi
 
+}
+
+function install_rapidjson() {
+    # ARG: rapidjson version
+    local rj_ver=$1
+    # test for ubuntu version, and check if we have a local package
+    local ubuntu_release=$(lsb_release -r | cut -f 2)
     # rapidjson is not available on Ubuntu 14.04 and too old on Ubuntu 16.04,
     # so we build from source
     if [[ $ubuntu_release =~ "16.04" ]]; then
         $SUDO apt-get remove -y rapidjson-dev
+    if [[ $ubuntu_release =~ "14.04" || $ubuntu_release =~ "16.04" ]]; then
+        build_rapidjson_from_source ${rj_ver} && \
+        $SUDO apt-get remove -y rapidjson-dev && \
+        $SUDO dpkg -i /tmp/RapidJSON-dev-${rj_ver}-Linux.deb || \
+            die "failed to install rapidjson"
+    else
+        $SUDO apt-get install -y rapidjson-dev || \
+            die "Failed to update rapidjson-dev"
     fi
-    builddir=$(mktemp --directory -t rjson_XXXXXX)
-    cd $builddir && \
-        git clone --recursive https://github.com/miloyip/rapidjson.git --branch "v1.1.0" && \
+}
+
+function build_rapidjson_from_source() {
+    # ARG: rapidjson version
+    local rj_ver=$1
+    local nprocs=$(cat /proc/cpuinfo | grep processor | wc -l)
+    local builddir=$(mktemp --directory -t rjson_XXXXXX)
+    cd $builddir
+    # add packaging info
+    cat > rj.patch <<EOF
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index ceda71b1..21acee18 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -171,3 +171,18 @@ INSTALL(FILES
+     \${CMAKE_CURRENT_BINARY_DIR}/\${PROJECT_NAME}ConfigVersion.cmake
+     DESTINATION "\${CMAKE_INSTALL_DIR}"
+     COMPONENT dev)
++
++# create a debian package
++set (CPACK_GENERATOR "DEB")
++set (CPACK_PACKAGE_NAME "\${PROJECT_NAME}-dev")
++set (CPACK_PACKAGE_VERSION_MAJOR \${LIB_MAJOR_VERSION})
++set (CPACK_PACKAGE_VERSION_MINOR \${LIB_MINOR_VERSION})
++set (CPACK_PACKAGE_VERSION_PATCH \${LIB_PATHC_VERSION})
++set (CPACK_PACKAGE_VERSION "\${LIB_VERSION_STRING}")
++set (CPACK_PACKAGE_CONTACT "THL A29 Limited")
++set (CPACK_PACKAGE_VENDOR "THL A29 Limited")
++set (CPACK_PACKAGE_DESCRIPTION_SUMMARY "RapidJSON")
++set (CPACK_PACKAGE_DESCRIPTION "RapidJSON")
++set (CPACK_DEBIAN_PACKAGE_ARCHITECTURE "amd64")
++include (CPack)
++
+EOF
+    git clone --recursive https://github.com/miloyip/rapidjson.git \
+        --branch "v${rj_ver}" && \
         cd rapidjson && \
+        git apply ../rj.patch && \
         mkdir build && cd build && \
-        cmake .. && \
+        cmake .. -DRAPIDJSON_BUILD_DOC=OFF \
+              -DRAPIDJSON_BUILD_EXAMPLES=OFF \
+              -DRAPIDJSON_BUILD_TESTS=OFF && \
         make -j $nprocs && \
-        $SUDO make install && \
+        cpack -G DEB && \
+        cp RapidJSON-dev-${rj_ver}-Linux.deb /tmp && \
         cd /tmp && \
-        rm -rf $builddir || die "Failed to install rapidjson"
+        rm -rf $builddir || die "Failed to build rapidjson"
 }
 
 install_macos_packages() {
