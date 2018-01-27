3d697fad4fc01089208ff27c61420d8aca298f74
Antonin Bas <antonin@barefootnetworks.com>
2018-01-26 17:25:40 -0800

    Add test P4 program for action data bug
    "constant value X out of range for immediate"

---
+++ p4-tests/p4_16/adata_constant_out_of_range_for_immediate.ptf/test.py
@@ -0,0 +1,16 @@
+import ptf.testutils as testutils
+
+from p4 import p4runtime_pb2
+
+from base_test import P4RuntimeTest, autocleanup
+from base_test import stringify
+
+class PacketUpdateTest(P4RuntimeTest):
+    def runTest(self):
+        port1 = self.swports(1)
+        inputV = 65536
+        outputV = inputV - 32
+        pkt = stringify(inputV, 2) + "\xab" * 64
+        exp_pkt = stringify(outputV, 2) + "\xab" * 64
+        testutils.send_packet(self, port1, str(pkt))
+        testutils.verify_packets(self, exp_pkt, [port1])
