031cbe1f58d3a5007d19c006860b5b38e31be69d
Jed Liu <jed-github.com@uma.litech.org>
2018-11-12 20:47:42 -0800

    Preserve original P4 functions for SaluAction nodes. (#2456)
    * Preserve P4 functions for `SaluAction` nodes.
    
    Preserve the original P4 functions when producing `SaluAction` nodes during back-end translation. This is not used by the back end, but is produced as part of the JSON dump so that a higher level representation of the function is available to tools that consume the JSON.

--- bf-p4c/mau/stateful_alu.cpp
+++ bf-p4c/mau/stateful_alu.cpp
@@ -174,7 +174,7 @@ bool CreateSaluInstruction::preorder(const IR::Function *func) {
     param_types = &function_param_types.at(std::make_pair(action_type_name, func->name));
     LOG3("Creating action " << name << "[" << func->id << "] for stateful table " << salu->name);
     LOG5(func);
-    action = new IR::MAU::SaluAction(func->srcInfo, name);
+    action = new IR::MAU::SaluAction(func->srcInfo, name, func);
     action->annotations = reg_action->annotations;
     outputs.clear();
     onebit = nullptr;
