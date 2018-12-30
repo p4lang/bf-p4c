db204a80fbfef5387f1ef81acb9dd8f3bad05ef3
Calin Cascaval <cascaval@barefootnetworks.com>
2018-12-30 14:56:12 -0800

    first release candidate for 8.6.0

--- bf-p4c/version.h
+++ bf-p4c/version.h
@@ -18,6 +18,6 @@ agreement with Barefoot Networks, Inc.
 #ifndef EXTENSIONS_BF_P4C_VERSION_H_
 #define EXTENSIONS_BF_P4C_VERSION_H_
 
-#define BF_P4C_VERSION "8.5.1"
+#define BF_P4C_VERSION "8.6.0-rc.1"
 
 #endif  // EXTENSIONS_BF_P4C_VERSION_H_
