883eda7a5696bfec59f10900b9de9e90449ebb7c
Jed Liu <jed-github.com@uma.litech.org>
2019-01-07 13:31:05 -0500

    Redirect docker_entry_point output to stderr. (#2667)
    This redirects the output of `docker_entry_point.sh` to stderr, so that when we invoke our containers to run the compiler, the result on stdout matches that of the compiler.

--- docker/docker_entry_point.sh
+++ docker/docker_entry_point.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # assumes that the Dockerfile will make sure that the script is at this location
-/bfn/ptf_hugepage_setup.sh
+/bfn/ptf_hugepage_setup.sh >&2
 # Setup veths
-/bfn/veth_setup.sh 34
+/bfn/veth_setup.sh 34 >&2
 # execute docker command
 exec "$@"
