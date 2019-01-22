490342436626c974fe437b04482ef0ff90d6eb97
Han Wang <hanw@users.noreply.github.com>
2019-01-21 17:51:41 -0800

    fix mirror.emit handling for headers (#2743)

--- bf-p4c/midend/elim_emit_headers.cpp
+++ bf-p4c/midend/elim_emit_headers.cpp
@@ -2,17 +2,23 @@
 #include "frontends/common/resolveReferences/resolveReferences.h"
 #include "frontends/p4/methodInstance.h"
 
-EliminateEmitHeaders::EliminateEmitHeaders(P4::ReferenceMap* refMap, P4::TypeMap* typeMap) :
+DoEliminateEmitHeaders::DoEliminateEmitHeaders(P4::ReferenceMap* refMap, P4::TypeMap* typeMap) :
     refMap(refMap), typeMap(typeMap) { setName("EliminateEmitHeaders"); }
 
-const IR::Node* EliminateEmitHeaders::preorder(IR::Argument *arg) {
-    if (!arg->expression->to<IR::PathExpression>())
-        return arg;
+const IR::Node* DoEliminateEmitHeaders::preorder(IR::Argument *arg) {
+    const IR::Type* type = nullptr;
+    if (auto path = arg->expression->to<IR::PathExpression>()) {
+        if (path->type->is<IR::Type_StructLike>())
+            type = path->type->to<IR::Type_StructLike>();
+        else
+            return arg;
+    } else if (auto mem = arg->expression->to<IR::Member>()) {
+        if (mem->type->is<IR::Type_StructLike>())
+            type = mem->type->to<IR::Type_StructLike>();
+        else
+            return arg; }
 
-    auto path = arg->expression->to<IR::PathExpression>();
-
-    if (!path->type->is<IR::Type_StructLike>())
-        return arg;
+    if (!type) return arg;
 
     auto mc = findContext<IR::MethodCallExpression>();
     if (!mc) return arg;
@@ -25,13 +31,13 @@ const IR::Node* EliminateEmitHeaders::preorder(IR::Argument *arg) {
             return arg; }
 
     auto fieldList = IR::Vector<IR::Expression>();
-    if (auto header = path->type->to<IR::Type_Header>()) {
+    if (auto header = type->to<IR::Type_Header>()) {
         for (auto f : header->fields) {
-            auto mem = new IR::Member(path, f->name);
+            auto mem = new IR::Member(arg->expression, f->name);
             fieldList.push_back(mem); }
-    } else if (auto st = path->type->to<IR::Type_Struct>()) {
+    } else if (auto st = type->to<IR::Type_Struct>()) {
         for (auto f : st->fields) {
-            auto mem = new IR::Member(path, f->name);
+            auto mem = new IR::Member(arg->expression, f->name);
             fieldList.push_back(mem); } }
 
     if (fieldList.size())
