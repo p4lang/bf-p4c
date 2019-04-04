32a9857e15ccc79511c8c274976855c31904ba24
Michael Attig <mattig@barefootnetworks.com>
2019-04-03 23:05:07 -0700

    Update to latest compiler-interfaces (#3029)

--- bf-asm/bfas.cpp
+++ bf-asm/bfas.cpp
@@ -17,7 +17,7 @@
 #define MAJOR_VERSION   1
 #define MINOR_VERSION   0
 
-const std::string SCHEMA_VERSION = "1.6.6";
+const std::string SCHEMA_VERSION = "1.6.7";
 
 option_t options = {
     .binary = PIPE0,
