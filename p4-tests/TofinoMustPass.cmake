835bb02a6e478d11ec8e32f244d2bcb1ec994cb8
Aashlesha Patil <41304909+AashleshaPatil-BFN@users.noreply.github.com>
2020-07-20 23:40:13 -0700

    P4C-2753: Add test to regression (#4375)
    * P4C-2753: Add test to regression
    
    P4C-2753 #assign #start-work Add test to regression
    
    * P4C-2753: Added test to proper place; modified TofinoMustPass
    
    P4C-2753 #assign #start-work Added test to proper place; modified TofinoMustPass
    
    * P4C-2753: Comment Offending statements
    
    P4C-2753 #assign #start-work Comment Offending statements

--- p4-tests/TofinoMustPass.cmake
+++ p4-tests/TofinoMustPass.cmake
@@ -125,6 +125,7 @@ set (TOFINO_MUST_PASS_TESTS
   extensions/p4_tests/p4_16/customer/kaloom/p4c-2410-spine.p4
   extensions/p4_tests/p4_16/customer/kaloom/p4c-2573-leaf.p4
   extensions/p4_tests/p4_16/customer/kaloom/p4c-2573-spine.p4
+  extensions/p4_tests/p4_16/customer/kaloom/p4c-2753.p4
   extensions/p4_tests/p4_16/customer/keysight/keysight-tf1.p4
   extensions/p4_tests/p4_16/customer/keysight/pktgen9_16.p4
   extensions/p4_tests/p4_14/customer/ruijie/p4c-2250.p4
