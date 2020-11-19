70b3e3b33a8dad9b1619a49f742a8a39df109609
Zhao Ma <30326088+zma-barefoot@users.noreply.github.com>
2020-11-19 09:35:37 -0800

    P4C-3171: pass now (#4716)
    P4C-3171 #assign #start-work pass now

--- p4-tests/JBayMustPass.cmake
+++ p4-tests/JBayMustPass.cmake
@@ -50,8 +50,7 @@ set (JBAY_MUST_PASS_TESTS
 # P4C-3300  
 # extensions/p4_tests/p4_16/customer/arista/obfuscated-msee_tofino2.p4
   extensions/p4_tests/p4_16/customer/arista/obfuscated-p416_baremetal_tofino2.p4
-# P4C-3171
-#  p4c-3171
+  p4c-3171
   extensions/p4_tests/p4_16/customer/keysight/keysight-tf2.p4
   # extensions/p4_tests/p4_16/customer/keysight/p4c-2554.p4
   )
