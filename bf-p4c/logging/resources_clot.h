338c51508f62e9ae1be72ad86e549e1c23d3188c
Jakub Neruda <66991058+jneruda@users.noreply.github.com>
2021-02-05 09:26:33 +0100

    P4C-3426: Change ClotUsage::issue_state into an array in resources.json (#4910)
    * P4C-3426: Change ClotUsage::issue_state into an array in resources.json
    
    CLOT tag can be issued by multiple mutually exclusive parser states. Multiple states might issue the same data and for such cases, the ClotUsage::issue_state was changed into an array. If state issues different data, then it is logged as a separate ClotUsage object.
    
    Also ClotUsage::field_lists was 2D array, but was used as only 1D array so it was flattened.
    
    P4C-3426 #assign jakub.neruda@intel.com

--- bf-p4c/logging/resources_clot.h
+++ bf-p4c/logging/resources_clot.h
@@ -16,6 +16,7 @@ class ClotResourcesLogging : public ParserInspector {
  public:
     using ClotResourceUsage = Resources_Schema_Logger::ClotResourceUsage;
     using ClotUsage = Resources_Schema_Logger::ClotUsage;
+    using ClotField = Resources_Schema_Logger::ClotField;
 
  protected:
     const ClotInfo &clotInfo;
@@ -23,18 +24,27 @@ class ClotResourcesLogging : public ParserInspector {
     std::vector<ClotResourceUsage*> clotUsages = std::vector<ClotResourceUsage*>(2);
     bool collected = false;
     std::map<unsigned, unsigned> clotTagToChecksumUnit;
+    std::vector<std::map<unsigned, std::vector<ClotUsage*>>> usageData;
 
     bool usingClots() const;
 
-    bool preorder(const IR::BFN::LoweredParserState* state);
-    void end_apply() override { collected = true; }
+    std::vector<ClotUsage*> &getUsageData(gress_t gress, unsigned tag);
 
-    void logClotUsages(const IR::BFN::LoweredParserMatch* match,
+    bool preorder(const IR::BFN::LoweredParserState* state);
+    void end_apply() override;
+
+    void collectClotUsages(const IR::BFN::LoweredParserMatch* match,
                        const IR::BFN::LoweredParserState* state,
                        gress_t gress);
 
-    ClotUsage* logExtractClotInfo(const IR::BFN::LoweredExtractClot* extract,
-                                  const IR::BFN::LoweredParserState *state);
+    void collectExtractClotInfo(const IR::BFN::LoweredExtractClot* extract,
+                                const IR::BFN::LoweredParserState *state,
+                                gress_t gress);
+
+    void logClotUsages();
+
+    ClotUsage* logExtractClotInfo(bool hasChecksum, int length, int offset,
+                                  unsigned tag, const Clot *clot);
 
  public:
     std::vector<ClotResourceUsage*> getLoggers();
