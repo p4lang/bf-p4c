0443355ec8464aa844e20855bdb579663fe541df
Vojtěch Havel <vojtech.havel@intel.com>
2022-01-28 18:58:19 +0100

    Upgrade dependencies  (#5704)
    * Update development environment
    
    * workaround to fix python2 tests
    
    P4factory doesn't officially support python2 since 9.7. To run python2
    ptf tests in our pipeline, we workaround py2 issues by manually
    altering p4factory dependency install scripts. This workaround broke in
    recent p4factory revisions where install scripts use python3-only syntax.
    
    As the python2 tests should be removed soon, this change freezes last
    working version of p4factory scripts used to install grpc and thrift
    and copies it into this repository.
    
    * regenerate driver outputs (linechanges only)
    
    * Revert "stop updating bfswitch refpoint (#5707)"
    
    This reverts commit cc90cf0c911a9fb24514800595036c381e4b271e.

--- scripts/run_custom_tests/run_with_python2.sh
+++ scripts/run_custom_tests/run_with_python2.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash -e
 
 bfnhome="/bfn/bf-p4c-compilers"
 
@@ -10,21 +10,24 @@ sed -i "/\s*os.environ\['PYTHONPATH'\].*os.path.join(testdir, site_pkg)/a \    o
 # Clear out CC and CXX (use local compilers instead of distcc)
 unset CC CXX
 
+
 # Install python2
 apt update && apt install -y python python-pip python-yaml
 pip2 install ipaddress crc16
 
-# Install grpc for python2
-sed -i 's/pip /pip2 /g' \
-        /p4factory/release/target-toplevel-files/p4studio/dependencies/source/install_grpc.py
-python2 /p4factory/release/target-toplevel-files/p4studio/dependencies/source/install_grpc.py \
-            -os Ubuntu -ver 16.04 -si /usr/local -j8
+pushd $bfnhome/scripts/run_custom_tests/p4studio_dependencies
 
+echo Installing grpc...
+# Install grpc for python2
+sed -i 's/pip /pip2 /g' install_grpc.py
+python install_grpc.py -os Ubuntu -ver 16.04 -si /usr/local -j8
+
+echo Installing thrift...
 # Install thrift for python2
-sed -i 's/pip /pip2 /g' \
-        /p4factory/release/target-toplevel-files/p4studio/dependencies/source/install_thrift.py
-python2 /p4factory/release/target-toplevel-files/p4studio/dependencies/source/install_thrift.py \
-            -os Ubuntu -ver 16.04 -si /usr/local -k apt -j8
+sed -i 's/pip /pip2 /g' install_thrift.py
+python install_thrift.py -os Ubuntu -ver 16.04 -si /usr/local -k apt -j8
+
+popd # p4studio_dependencies
 
 pip2 install ptf scapy
 
