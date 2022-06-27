947f4afeb7157af1fb3491e32bcaf16035820c3d
Prathima Kotikalapudi <pkotikalapudi@barefootnetworks.com>
2022-06-27 11:54:33 -0700

    Remove z2_tofino2 and z2_tofino3 profiles corresponding to switch update. (#6153)
    [no-jira]

--- p4-tests/SwitchCloudbreak.cmake
+++ p4-tests/SwitchCloudbreak.cmake
@@ -9,11 +9,5 @@ file (RELATIVE_PATH switch_p4_16_y2 ${P4C_SOURCE_DIR} ${SWITCH_P4_16_Y2})
 p4c_add_test_with_args("tofino3" ${P4C_RUNTEST} FALSE
  "smoketest_switch_16_compile_y2_profile" ${switch_p4_16_y2} "${testExtraArgs}" "-DY2_PROFILE -I${SWITCH_P4_16_INC} -tofino3 -Xp4c=\"--auto-init-metadata\" -arch t3na")
 
-#set (SWITCH_P4_16_Z2 ${SWITCH_P4_16_ROOT}/p4src/switch-tofino2/switch_tofino2_z2.p4)
-#file (RELATIVE_PATH switch_p4_16_z2 ${P4C_SOURCE_DIR} ${SWITCH_P4_16_Z2})
-#p4c_add_test_with_args("tofino3" ${P4C_RUNTEST} FALSE
-#  "smoketest_switch_16_compile_z2_profile" ${switch_p4_16_z2} "${testExtraArgs}" "-DZ2_PROFILE -I${SWITCH_P4_16_INC} -tofino3 -Xp4c=\"--auto-init-metadata -Tstage_advance:1\" -arch t3na --num-stages-override 16")
-#
 ## 500s timeout is too little for compiling and testing the entire switch, bumping it up
 set_tests_properties("tofino3/smoketest_switch_16_compile_y2_profile" PROPERTIES TIMEOUT ${extended_timeout_2times})
-#set_tests_properties("tofino3/smoketest_switch_16_compile_z2_profile" PROPERTIES TIMEOUT ${extended_timeout_2times})
