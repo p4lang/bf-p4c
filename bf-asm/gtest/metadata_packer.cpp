126ed76629582efef922a21504fde364b6fb109a
Glen Gibb <glen.gibb@intel.com>
2022-07-14 18:24:08 +1000

    P4C-4676: verify bridge metadata doesn't overwrite hdr ptrs (#6220)
    Verify that the bridge metadata start position is >= the minimum size of
    the header pointers.

--- bf-asm/gtest/metadata_packer.cpp
+++ bf-asm/gtest/metadata_packer.cpp
@@ -85,5 +85,32 @@ deparser ingress:
 }
 */
 
+// Verify that the bridge metadata is not overwriting header pointers
+TEST(metadata_packer, bridge_meta_start) {
+    const char* deparser_str = R"MDP_CFG(
+version:
+  target: Tofino5
+deparser ingress:
+  dictionary: {}
+)MDP_CFG";
+
+    // Minimum start position if we allow up to 10 pointers:
+    //   - Byte 0: 0 (start hdr offset)
+    //   - Byte 1: 0xFF (not compressed)
+    //   - Byte 2-11: header ids
+    //   - Byte 12-21: compressed header lengths (if using -- not in use yet)
+    int min_start = 1 + 1 + 10;
+
+    resetTarget();
+
+    asm_parse_string(deparser_str);
+
+    Target::Flatrock::deparser_regs regs;
+    Deparser* dprsr = dynamic_cast<Deparser*>(Section::test_get("deparser"));
+    dprsr->write_config(regs);
+
+    EXPECT_GE(regs.mdp_mem.rem_brm_ext_ram.rem_brm_ext[0].rem_brm_start, min_start);
+}
+
 }  // namespace
 
