2dec771fc83c7c2d6ad4ab07436a25dc80d2610f
Vladimír Štill <vladimir.still@intel.com>
2022-10-13 13:15:48 +0200

    Set default hugepages to 192 and makes the script more robust (#6563)

--- scripts/ptf_hugepage_setup.sh
+++ scripts/ptf_hugepage_setup.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-if [ ! -d /mnt/huge ]; then
-    echo "This is the docker entry point"
-    if [ -z "$NUM_HUGEPAGES" ]; then
-        echo "vm.nr_hugepages = 128" >> /etc/sysctl.conf
+HUGE_DEFAULT=192
+HUGE_TO_SET=${NUM_HUGEPAGES:-${1:-$HUGE_DEFAULT}}
+HUGE_KEY=vm.nr_hugepages
+HUGE_ACTUAL=$(sysctl --values $HUGE_KEY)
+CONF=/etc/sysctl.conf
+
+if [[ $HUGE_TO_SET -gt $HUGE_ACTUAL ]]; then
+    if grep -q "^$HUGE_KEY\>" $CONF; then
+        sed -i "s/^$HUGE_KEY\>.*$/$HUGE_KEY = $HUGE_TO_SET/" $CONF
     else
-        echo "vm.nr_hugepages = $NUM_HUGEPAGES" >> /etc/sysctl.conf
+        echo "$HUGE_KEY = $HUGE_TO_SET" >> $CONF
     fi
     sysctl -p /etc/sysctl.conf
+fi
+if [[ ! -d /mnt/huge ]]; then
     mkdir /mnt/huge
     mount -t hugetlbfs nodev /mnt/huge
     echo -e "nodev /mnt/huge hugetlbfs\n" >> /etc/fstab
