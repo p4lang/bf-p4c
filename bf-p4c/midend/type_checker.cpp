ae274d7b74fc83eb51ec59840bdd52244dc43ea7
Matej Vido <matej.vido@intel.com>
2022-11-24 12:50:21 +0100

    Remove unused pass StructExprToListExpr (#6699)
    The pass is not used since PR:
    https://github.com/intel-restricted/networking.switching.barefoot.bf-p4c-compilers/pull/6164
    so it can be removed.
    
    [no-jira]

--- bf-p4c/midend/type_checker.cpp
+++ bf-p4c/midend/type_checker.cpp
@@ -109,49 +109,6 @@ TypeChecking::TypeChecking(P4::ReferenceMap* refMap, P4::TypeMap* typeMap,
     setStopOnError(true);
 }
 
-// Convert StructExpressions to ListExpressions
-class StructExprToListExpr: public Transform {
-    P4::ReferenceMap* refMap;
-    P4::TypeMap* typeMap;
-
- public:
-    StructExprToListExpr(P4::ReferenceMap* refMap, P4::TypeMap* typeMap) :
-        refMap(refMap), typeMap(typeMap) {}
-
-    const IR::Node* preorder(IR::StructExpression *se) {
-        // Convert only checksums/hashes
-        auto mc = findContext<IR::MethodCallExpression>();
-        if (!mc) return se;
-        auto method = mc->method->to<IR::Member>();
-        if (!method || method->member == "subtract_all_and_deposit") return se;
-        auto mi = P4::MethodInstance::resolve(mc, refMap, typeMap, true);
-        auto em = mi->to<P4::ExternMethod>();
-        if (!em) return se;
-        cstring extName = em->actualExternType->name;
-        if (extName != "Checksum" && extName != "Hash") return se;
-
-        auto list = IR::Vector<IR::Expression>();
-        for (auto comp : se->components) {
-            list.push_back(comp->expression);
-        }
-        return new IR::ListExpression(se->srcInfo, list);
-    }
-
-    const IR::Node* preorder(IR::HashStructExpression *hse) {
-        auto list = IR::Vector<IR::Expression>();
-        for (auto comp : hse->components) {
-            list.push_back(comp->expression);
-        }
-        auto hle = new IR::HashListExpression(hse->srcInfo,
-                                              list,
-                                              hse->fieldListCalcName,
-                                              hse->outputWidth);
-        hle->fieldListNames = hse->fieldListNames;
-        hle->algorithms = hse->algorithms;
-        return hle;
-    }
-};
-
 // similarly, it might be better to avoid code duplication here.
 EvaluatorPass::EvaluatorPass(P4::ReferenceMap* refMap, P4::TypeMap* typeMap) {
     evaluator = new P4::Evaluator(refMap, typeMap);
