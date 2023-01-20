0f80f8896e11a4a315a69c697036dea40db96d6e
Marek Sedlacek <marek.sedlacek@intel.com>
2023-01-20 13:41:58 +0100

    P4C-4921 Benchmarking scripts improvements (#6808)
    * P4C-4921: Added -t option to specify subset of test to run
    * P4C-4921: Added -r option and averaging; Fixed path to be Path object and not string
    * P4C-4921: Added option to run benchmarks multiple times and output the average result; Doxygen compatibility; Code refactor; Cloning and building the compiler for specified commit.
    * P4C-4921: Added option to benchmark specific commit
    * P4C-4921: Added -j and -b option as per request and -x option.

--- p4-tests/benchmarks/bf-p4c-benchmarks.py
+++ p4-tests/benchmarks/bf-p4c-benchmarks.py
@@ -13,21 +13,41 @@ import psutil
 import re
 from datetime import datetime
 import sys
+from pathlib import Path
 
 # Version is needed to correctly compare and plot outputs.
 # If any major change is done to the results, the version should change.
 __version__ = "1.0.0"
+verbose = False
 
 def fatal_error(msg):
-    """
+    """!
     Prints error message and exits with 1
     :param msg Error message
     """
     print("ERROR: {}".format(msg), file=sys.stderr)
     exit(1)
 
-class BenchmarkTest:
+
+def run_shell_cmd(cmd):
+    """!
+    Runs a shell command
+    @param cmd Command to be run as a string or a list
+    @return Object containing:
+            - returncode: return code
+            - stdout: standard output after running the command
+            - stderr: standard output error after running the command
     """
+    # Concat command and args
+    if type(cmd) != str:
+        cmd = " ".join(cmd)
+    if verbose:
+        print("+", cmd)
+    return subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
+
+
+class BenchmarkTest:
+    """!
     Holds information and setting for a test
     """
     # Regex for extracting measured time
@@ -41,40 +61,36 @@ class BenchmarkTest:
         :param compiler_path Path to P4C compiler
         """
         self.name = name
-        self.compiler_path = compiler_path
+        self.compiler_path = Path(compiler_path)
         self.dir_path = dir_path
-        self.path = params["path"]
+        self.path = Path(params["path"])
         self.args = params["args"]
         # Go though args and those starting with "./" change to path to this folder
         for i in range(len(self.args)):
             if len(self.args[i]) > 1 and self.args[i][0:2] == "./":
-                self.args[i] = self.dir_path+self.args[i][1:]
+                self.args[i] = self.dir_path / Path(self.args[i][1:])
         self.core_number = core_number
 
     def run(self):
-        """
+        """!
         Runs benchmark test
         """
-        measure_script = self.dir_path+"/measure_process.sh"
+        measure_script = self.dir_path / Path("measure_process.sh")
         core_number = self.core_number
-        p4c_args = " ".join(self.args)
-        test_path = self.dir_path+"/"+self.path
+        p4c_args = " ".join(str(x) for x in self.args)
+        test_path = self.dir_path / self.path
 
-        command = " ".join(["bash", measure_script, core_number, '"', self.compiler_path, p4c_args, 
-                            test_path, "-o /tmp/.benchmark_output", '"'])
-        result = subprocess.Popen([command],
-                                  shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        result_stderr = result.communicate()[1].decode("utf-8")
+        command = " ".join(["bash", str(measure_script), core_number, '"', str(self.compiler_path), p4c_args, 
+                            str(test_path), "-o /tmp/.benchmark_output", '"'])
+        result = run_shell_cmd(command)
         if result.returncode != 0:
-            fatal_error("Command '{}' exited with {}!"
-                        .format(command, result.returncode))
-        time_str = BenchmarkTest.RE_TIME_MEASURED.search(result_stderr)
-        # TODO: Handle error?
+            fatal_error(f"Command '{command}' exited with {result.returncode}!")
+        time_str = BenchmarkTest.RE_TIME_MEASURED.search(result.stderr)
         time = float(time_str.groups()[0])
         return time
 
 class Benchmarks:
-    """
+    """!
     Class handeling benchmarks
     """
     # Regex for extracting p4c version
@@ -82,7 +98,7 @@ class Benchmarks:
     RE_P4C_SHA = re.compile(r'p4c [0-9]+\.[0-9]+\.[0-9]+ \(SHA: (.*)\)')
 
     def __init__(self, arg_parser):
-        """
+        """!
         Constructor
         :param arg_parser argparse object
         """
@@ -90,18 +106,29 @@ class Benchmarks:
         # Parse arguments
         self.add_command_line_options()
         self.opts = self._arg_parser.parse_args()
-
         self.p4c_path = self.opts.p4c_path
         self.core_number = self.opts.core_number
         self.output_path = self.opts.output_path
-        self.tests_dir = os.path.dirname(self.opts.tests_path)
+        self.repeat = self.opts.repeat
+        self.repo_path = self.opts.repo_path
+        self.make_cores = self.opts.make_cores
+        global verbose
+        verbose = self.opts.verbose
+        self.build_command = "./bootstrap_bfn_compilers.sh --build-dir build --small-config --build-type RelWithDebInfo -DENABLE_GTESTS=OFF -DENABLE_BMV2=OFF -DENABLE_DPDK=OFF"
+        if self.opts.build_path is not None:
+            with open(self.opts.build_path, "r", encoding="utf-8") as build_script:
+                self.build_command = build_script.read()
+        # Flatten test_filter and commits
+        self.test_filter = None if self.opts.test_filter is None else [x for subl in self.opts.test_filter for x in subl]
+        self.commits = None if self.opts.commits is None else [x for subl in self.opts.commits for x in subl]
+        self.tests_dir = Path(os.path.dirname(self.opts.tests_path))
         print(self.tests_dir)
         # Load tests from config into dict
         self.tests_config = self.load_json(self.opts.tests_path)
         self.reference = self.load_json(self.opts.ref_file) if self.opts.ref_file is not None else None
 
     def add_command_line_options(self):
-        """
+        """!
         Sets up command line options for benchmarks
         """
         self._arg_group = self._arg_parser.add_argument_group("P4C benchmarks options")
@@ -120,12 +147,39 @@ class Benchmarks:
                                     action="store", default=None, type=str,
                                     dest="ref_file", required=False)
         self._arg_group.add_argument("-o",
-                                    help="File to which save the output results.",
+                                    help="File to which save the output json results.",
                                     action="store", default=None, type=str,
                                     dest="output_path", required=False)
+        self._arg_group.add_argument("-d",
+                                     help="Destination to which clone p4c repo or where it already \
+                                     is cloned.", default="/tmp/bf-p4c-compilers", type=str,
+                                     dest="repo_path", required=False)
+        self._arg_group.add_argument("-t",
+                                    help="Specific test(s) to run",
+                                    action="append", default=None, nargs='+',
+                                    dest="test_filter", required=False)
+        self._arg_group.add_argument("-r", "--repeat",
+                                    help="Run the same test set given times.",
+                                    action="store", default=1, type=int,
+                                    dest="repeat", required=False)
+        self._arg_group.add_argument("-c",
+                                    help="Specific commit(s) to benchmark",
+                                    action="append", default=None, nargs='+',
+                                    dest="commits", required=False)
+        self._arg_group.add_argument("-j",
+                                    help="Number of cores to be used for make command.",
+                                    action="store", default=76, type=int,
+                                    dest="make_cores", required=False)
+        self._arg_group.add_argument("-b", "--build",
+                                    help="Path to a build (bootstrap) script to be used.",
+                                    action="store", default=None,
+                                    dest="build_path", required=False)
+        self._arg_group.add_argument("-x",
+                                    help="Verbose. Prints out executed shell commands.",
+                                    action="store_true", default=False, dest="verbose")
 
     def load_json(self, path):
-        """
+        """!
         Loads json from passed in path
         :param path Path to the json file
         :return JSON file as a python dict
@@ -137,15 +191,13 @@ class Benchmarks:
 
     @staticmethod
     def get_platform_info():
-        """
+        """!
         :return Dict with info about this platform (PC)
         """
         # Getting CPU model
-        cpu_res = subprocess.Popen(["lscpu | grep 'Model name:.*'"],
-                                    shell=True, stdout=subprocess.PIPE)
-        cpu_res_stdou = cpu_res.communicate()[0].decode('utf-8')
+        cpu_res = run_shell_cmd(["lscpu | grep 'Model name:.*'"])
         # Remove prefix from lscpu to get the cpu model
-        cpu_model = re.sub(r'Model name:\s*', '', cpu_res_stdou)
+        cpu_model = re.sub(r'Model name:\s*', '', cpu_res.stdout)
         cpu_model = cpu_model.rstrip()
         return {
             "memory": {
@@ -162,21 +214,70 @@ class Benchmarks:
 
     @staticmethod
     def get_p4c_info(p4c_path):
-        """
+        """!
         :return Dict with info about used P4C compiler
         """
-        p4c_res = subprocess.Popen(["{} --version".format(p4c_path)],
-                                    shell=True, stdout=subprocess.PIPE)
-        p4c_res_stdou = p4c_res.communicate()[0].decode('utf-8')
-        p4c_version_match = Benchmarks.RE_P4C_VERSION.search(p4c_res_stdou)
-        p4c_sha_match = Benchmarks.RE_P4C_SHA.search(p4c_res_stdou)
+        p4c_res = run_shell_cmd(["{} --version".format(p4c_path)]) 
+        p4c_version_match = Benchmarks.RE_P4C_VERSION.search(p4c_res.stdout)
+        p4c_sha_match = Benchmarks.RE_P4C_SHA.search(p4c_res.stdout)
         return {
             "version": p4c_version_match.groups()[0],
             "sha": p4c_sha_match.groups()[0]
         }
 
-    def compare(self, results, reference):
+    def clone_repo(self, dst_dir):
+        """!
+        Clones the bf-p4c repo to dst_dir, unless it already exists
+        @param dst_dir Destination directory for the bf-p4c compiler repo
+        @return None
         """
+        if not Path(dst_dir).is_dir():
+            cmd = ["git", "clone", "--recursive", 
+                "git@github.com:barefootnetworks/bf-p4c-compilers.git", dst_dir]
+            print("Cloning bf-p4c repo, this might take some time...")
+            result = run_shell_cmd(cmd)
+            if result.returncode != 0:
+                print(f"Error occurred when cloning bf-p4c: \"{result.stderr}\"", file=sys.stderr)
+                exit(1)
+        else:
+            print("Destination directory already exists. Repository will not be cloned.")
+        
+    def checkout(self, commit, dir):
+        """!
+        Checksout bf-p4c repo to passed in commit
+        @param commit Commit to checkout to
+        @param dir Directory where is the bf-p4c repo cloned
+        @return None
+        """
+        cmd = ["cd", dir, "&&", "git", "checkout", commit, "&&", "git", "submodule", "update"]
+        print(f"Checking out to {commit}.")
+        result = run_shell_cmd(cmd)
+        if result.returncode != 0:
+            print(f"Error checking out the repository: \"{result.stderr}\"", file=sys.stderr)
+            exit(1)
+
+    def build(self, dir):
+        """!
+        Builds bf-p4c
+        @param dir Directory, where the repo is located
+        @return None
+        """
+        cmd = ["cd", dir, "&&", self.build_command]
+        print("Running a bootstrap")
+        result = run_shell_cmd(cmd)
+        if result.returncode != 0:
+            print(f"Error occurred when running a bootstrap: '{result.stderr}'", file=sys.stderr)
+            exit(1)
+        cmd = ["cd", dir, "&&", "cd", "build", "&&", "make", "-j"+str(self.make_cores)]
+        print("Running make, this might take some time...")
+        result = run_shell_cmd(cmd)
+        if result.returncode != 0:
+            print(f"Error occurred when making the compiler: '{result.stderr}'", file=sys.stderr)
+            exit(1)
+
+
+    def compare(self, results, reference):
+        """!
         Compares benchmark results with reference values and prints
         the difference in human readable form to the stdout
         :param results Benchmark results dict
@@ -208,9 +309,24 @@ class Benchmarks:
                       .format(name, diff_abs, c_time_master, c_time_now))
         return in_thr
 
+    @staticmethod
+    def average_results(results):
+        """!
+        Takes list of results and returns single result containing
+        average values for all results.
+        :param results List of result dictionaries
+        :return Result dictionary with average value for given list of results
+        """
+        avg = {}
+        for test_name in results[0]:
+            sum = 0
+            for r in results:
+                sum += r[test_name]["compile_time"]
+            avg[test_name] = sum/len(results)
+        return avg
 
     def save_results(self, results, output_path=None):
-        """
+        """!
         Saves benchmarks results and needed info into a file
         :param results Results dictionary
         :param output_path Path to the output file or folder, if None, then
@@ -236,7 +352,7 @@ class Benchmarks:
             json.dump(output_dict, json_f, indent=2)
 
     def run_benchmarks(self):
-        """
+        """!
         Runs P4C benchmark pipeline
         :return True if benchmark results are within set compare threshold or 
                 no compare file was set. False otherwise.
@@ -244,24 +360,51 @@ class Benchmarks:
         was_success = True
         tests = []
         for test, params in self.tests_config.items():
-            tests.append(BenchmarkTest(test, params, self.p4c_path, self.tests_dir, self.core_number))
+            if self.test_filter is None or test in self.test_filter:
+                tests.append(BenchmarkTest(test, params, self.p4c_path, self.tests_dir, self.core_number))
 
-        results = {}
-        for c, test in enumerate(tests):
-            print("[{}/{}] RUNNING {}".format(c+1, len(tests), test.name))
-            compile_time = test.run()
-            results[test.name] = {"compile_time": compile_time,
-                                  "path": test.path,
-                                  "p4c_args": test.args}
-            print("[{}/{}] FINISHED {} in {} sec".format(c+1, len(tests), test.name, compile_time))
+        all_results = []
+        for _ in range(self.repeat):
+            results = {}
+            for c, test in enumerate(tests):
+                print("[{}/{}] RUNNING {}".format(c+1, len(tests), test.name))
+                compile_time = test.run()
+                results[test.name] = {"compile_time": compile_time,
+                                    "path": test.path,
+                                    "p4c_args": test.args}
+                print("[{}/{}] FINISHED {} in {} sec".format(c+1, len(tests), test.name, compile_time))
+            all_results.append(results)
+        # Calculate average result from all runs
+        avg_results = Benchmarks.average_results(all_results)
         # Save results to the output file
-        self.save_results(results, self.output_path)
+        self.save_results(avg_results, self.output_path)
         # Compare results with reference values
         if self.reference is not None:
             was_success = self.compare(results, self.reference)
         return was_success
 
+    def start(self):
+        """!
+        Starts benchmarking
+        """
+        was_success = True
+        # TODO: Put the commit name/number into the output file name
+        if self.commits is not None and len(self.commits) > 0:
+            self.clone_repo(self.repo_path)
+            for c in self.commits:
+                self.checkout(c, self.repo_path)
+                self.build(self.repo_path)
+                # Set p4c path to the newly build repo
+                self.p4c_path = Path(self.repo_path) / Path("build/p4c/p4c")
+                # Run benchmarks and save if one of them failed
+                if not self.run_benchmarks():
+                    was_success = False
+        else:
+            was_success = self.run_benchmarks()
+
+        return was_success
+
 if __name__ == "__main__":
     benchmarks = Benchmarks(argparse.ArgumentParser(description="Runs P4C performance benchmarks."))
-    was_success = benchmarks.run_benchmarks()
+    was_success = benchmarks.start()
     sys.exit(0 if was_success else 1)
