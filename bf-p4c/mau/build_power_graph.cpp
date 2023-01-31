48b98a4a6bb6ca85af5e4f7284a2da29ab066b9a
Chris Dodd <chris.dodd@intel.com>
2023-01-31 15:24:17 -0800

    Fix worst_path memoization in visit_power_graph (#6843)
    * Mark tables considered always-run for Power estimation due to JBAY-2889 with an "*"
    in the mau.power.log
    [no-jira]
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch jbay-2889
    # Changes to be committed:
    #	modified:   bf-p4c/mau/walk_power_graph.cpp
    #	modified:   bf-p4c/mau/walk_power_graph.h
    #
    # Changes not staged for commit:
    #	modified:   p4-tests/p4-programs (untracked content)
    #	modified:   p4-tests/p4_16/switch_16 (new commits)
    #
    # Untracked files:
    #	3
    #	bf-sde-9.7.3.191-cpr.tgz
    #	bf-sde-9.7.3.191-cpr/
    #	bfas.config.log
    #	git.log
    #	gitlog
    #	p4o-new.p4
    #	p4o.fail/
    #	patch
    #	switch_tofino_x7.p4pp
    #	tna_ternary_match.p4info
    #
    
    * P4C-5022: Add power logging for edges
    
    * P4C-5022: fix clang-format and improve logging
    
    * P4C-5022: memoize worst_path correctly in SimplePowerGraph::visit_node_power
    
    ---------
    
    Co-authored-by: Amresh Kulkarni <amresh.kulkarni@intel.com>

--- bf-p4c/mau/build_power_graph.cpp
+++ bf-p4c/mau/build_power_graph.cpp
@@ -1,5 +1,5 @@
-#include "bf-p4c/bf-p4c-options.h"
 #include "bf-p4c/mau/build_power_graph.h"
+#include "bf-p4c/bf-p4c-options.h"
 #include "bf-p4c/mau/default_next.h"
 #include "bf-p4c/mau/jbay_next_table.h"
 #include "bf-p4c/mau/mau_power.h"
@@ -10,124 +10,122 @@
 
 namespace MauPower {
 
-BuildPowerGraph::BuildPowerGraph(const NextTable* next_table_properties,
-                                 const BFN_Options& options) :
-  next_table_properties_(next_table_properties), options_(options) {
-  ingress_graph_ = new SimplePowerGraph(toString(INGRESS));
-  egress_graph_ = new SimplePowerGraph(toString(EGRESS));
-  ghost_graph_ = new SimplePowerGraph(toString(GHOST));
+BuildPowerGraph::BuildPowerGraph(const NextTable *next_table_properties, const BFN_Options &options)
+    : next_table_properties_(next_table_properties), options_(options) {
+    ingress_graph_ = new SimplePowerGraph(toString(INGRESS));
+    egress_graph_ = new SimplePowerGraph(toString(EGRESS));
+    ghost_graph_ = new SimplePowerGraph(toString(GHOST));
 }
 
-SimplePowerGraph* BuildPowerGraph::get_graph(gress_t g) {
-  if (g == EGRESS)
-    return egress_graph_;
-  else if (g == GHOST)
-    return ghost_graph_;
-  else
-    return ingress_graph_;
+SimplePowerGraph *BuildPowerGraph::get_graph(gress_t g) {
+    if (g == EGRESS)
+        return egress_graph_;
+    else if (g == GHOST)
+        return ghost_graph_;
+    else
+        return ingress_graph_;
 }
 
 Visitor::profile_t BuildPowerGraph::init_apply(const IR::Node *root) {
-  return MauInspector::init_apply(root);
+    return MauInspector::init_apply(root);
 }
 
 void BuildPowerGraph::end_apply(const IR::Node *root) {
-  if (options_.debugInfo) {  // graph outputs
-    for (gress_t g : Device::allGresses()) {
-      SimplePowerGraph* graph = get_graph(g);
-      std::string fname = graph->name_ + ".power.dot";
-      auto logDir = BFNContext::get().getOutputDirectory("graphs",
-                                                         root->to<IR::BFN::Pipe>()->canon_id());
-      if (logDir)
-        fname = logDir + "/" + graph->name_ + ".power.dot";
-      graph->to_dot(fname);
+    if (options_.debugInfo) {  // graph outputs
+        for (gress_t g : Device::allGresses()) {
+            SimplePowerGraph *graph = get_graph(g);
+            std::string fname = graph->name_ + ".power.dot";
+            auto logDir = BFNContext::get().getOutputDirectory(
+                "graphs", root->to<IR::BFN::Pipe>()->canon_id());
+            if (logDir) fname = logDir + "/" + graph->name_ + ".power.dot";
+            graph->to_dot(fname);
+        }
     }
-  }
 }
 
 bool BuildPowerGraph::preorder(const IR::MAU::TableSeq *seq) {
-  LOG4("\nTable seq");
-  for (auto t : seq->tables) {
-    LOG4("  t = " << t->externalName());
-  }
-
-  for (auto t : seq->tables) {
-    SimplePowerGraph* graph = get_graph(t->gress);
-    Node* root = graph->get_root();
-    if (root->out_edges_.size() == 0) {
-      graph->add_connection(root->unique_id_, {t->unique_id()}, "$start");
-      break;
+    LOG4("\nTable seq");
+    for (auto t : seq->tables) {
+        LOG4("  t = " << t->externalName());
     }
-  }
-  return true;
+
+    for (auto t : seq->tables) {
+        SimplePowerGraph *graph = get_graph(t->gress);
+        Node *root = graph->get_root();
+        if (root->out_edges_.size() == 0) {
+            graph->add_connection(root->unique_id_, {t->unique_id()}, "$start");
+            break;
+        }
+    }
+    return true;
 }
 
 bool BuildPowerGraph::preorder(const IR::MAU::Table *tbl) {
-  SimplePowerGraph* graph = get_graph(tbl->gress);
-  LOG4("Table " << tbl->externalName());
-  switch (tbl->always_run) {
-  case IR::MAU::AlwaysRun::TABLE:
-    LOG4("  always run!");
-    if (always_run_.size() > 0) {
-      UniqueId last = always_run_.back();
-      std::vector<UniqueId> leaves;
-      graph->get_leaves(last, leaves);
-      for (auto u : leaves) {
-        graph->add_connection(u, {tbl->unique_id()}, "$always");
-      }
+    SimplePowerGraph *graph = get_graph(tbl->gress);
+    LOG4("Table " << tbl->externalName());
+    switch (tbl->always_run) {
+        case IR::MAU::AlwaysRun::TABLE:
+            LOG4("  always run!");
+            if (always_run_.size() > 0) {
+                UniqueId last = always_run_.back();
+                std::vector<UniqueId> leaves;
+                graph->get_leaves(last, leaves);
+                for (auto u : leaves) {
+                    graph->add_connection(u, {tbl->unique_id()}, "$always");
+                }
+            }
+            always_run_.push_back(tbl->unique_id());
+            break;
+
+        case IR::MAU::AlwaysRun::NONE:
+            break;
+
+        case IR::MAU::AlwaysRun::ACTION:
+            return true;
     }
-    always_run_.push_back(tbl->unique_id());
-    break;
 
-  case IR::MAU::AlwaysRun::NONE:
-    break;
+    // Build connections by looking at action names, $hit, $miss, $try_next_stage, $default.
+    LOG4("Actions:");
+    for (auto a : tbl->actions) {
+        ordered_set<UniqueId> next_edges = next_for(tbl, a.second->name.originalName);
+        LOG4("  " << a.second->name.originalName << ": " << next_edges.size());
+        for (auto u : next_edges) LOG4("    " << u);
+        graph->add_connection(tbl->unique_id(), next_edges, a.second->name.originalName);
+    }
+    for (auto &line : tbl->gateway_rows) {
+        if (line.second) {
+            ordered_set<UniqueId> gw_edges = next_for(tbl, line.second);
+            if (gw_edges.size() > 0) {
+                LOG4("GW edge: " << line.second);
+                for (auto g : gw_edges) LOG4("    " << g);
+                graph->add_connection(tbl->unique_id(), gw_edges, line.second);
+            }
+        }
+    }
 
-  case IR::MAU::AlwaysRun::ACTION:
+    std::vector<cstring> edge_types = {"$hit", "$miss", "$try_next_stage", "$default"};
+    for (cstring edge_name : edge_types) {
+        ordered_set<UniqueId> found_edges = next_for(tbl, edge_name);
+        if (found_edges.size() > 0) {
+            LOG4(edge_name << ": " << found_edges.size());
+            for (auto u : found_edges) LOG4("    " << u);
+            graph->add_connection(tbl->unique_id(), found_edges, edge_name);
+        }
+    }
     return true;
-  }
-
-  // Build connections by looking at action names, $hit, $miss, $try_next_stage, $default.
-  LOG4("Actions:");
-  for (auto a : tbl->actions) {
-    ordered_set<UniqueId> next_edges = next_for(tbl, a.second->name.originalName);
-    LOG4("  " << a.second->name.originalName << ": " << next_edges.size());
-    for (auto u : next_edges)
-      LOG4("    " << u);
-    graph->add_connection(tbl->unique_id(), next_edges, a.second->name.originalName);
-  }
-  for (auto &line : tbl->gateway_rows) {
-    if (line.second) {
-      ordered_set<UniqueId> gw_edges = next_for(tbl, line.second);
-      if (gw_edges.size() > 0) {
-        LOG4("GW edge: " << line.second);
-        for (auto g : gw_edges)
-          LOG4("    " << g);
-        graph->add_connection(tbl->unique_id(), gw_edges, line.second);
-  } } }
-
-  std::vector<cstring> edge_types = { "$hit", "$miss", "$try_next_stage", "$default" };
-  for (cstring edge_name : edge_types) {
-    ordered_set<UniqueId> found_edges = next_for(tbl, edge_name);
-    if (found_edges.size() > 0) {
-      LOG4(edge_name << ": " << found_edges.size());
-      for (auto u : found_edges)
-        LOG4("    " << u);
-      graph->add_connection(tbl->unique_id(), found_edges, edge_name);
-  } }
-  return true;
 }
 
-BuildPowerGraph* BuildPowerGraph::clone() const {
-  LOG5(" ------ clone ----- ");
-  return new BuildPowerGraph(*this);
+BuildPowerGraph *BuildPowerGraph::clone() const {
+    LOG5(" ------ clone ----- ");
+    return new BuildPowerGraph(*this);
 }
-void BuildPowerGraph::flow_merge(Visitor& /* v */) {
-  LOG5(" ------ merge ----- ");
-  // nothing to do yet.
+void BuildPowerGraph::flow_merge(Visitor & /* v */) {
+    LOG5(" ------ merge ----- ");
+    // nothing to do yet.
 }
 
 ordered_set<UniqueId> BuildPowerGraph::next_for(const IR::MAU::Table *tbl, cstring what) const {
-  return next_table_properties_->next_for(tbl, what);
+    return next_table_properties_->next_for(tbl, what);
 }
 
 };  // end namespace MauPower
--- bf-p4c/mau/build_power_graph.h
+++ bf-p4c/mau/build_power_graph.h
@@ -1,50 +1,49 @@
 #ifndef BF_P4C_MAU_BUILD_POWER_GRAPH_H_
 #define BF_P4C_MAU_BUILD_POWER_GRAPH_H_
 
-#include "mau_visitor.h"
 #include "bf-p4c/bf-p4c-options.h"
 #include "bf-p4c/ir/control_flow_visitor.h"
 #include "bf-p4c/mau/default_next.h"
 #include "bf-p4c/mau/jbay_next_table.h"
 #include "bf-p4c/mau/simple_power_graph.h"
 #include "lib/ordered_set.h"
+#include "mau_visitor.h"
 
 namespace MauPower {
 
 /**
-  * This class constructs the final placed MAU table control flow graph for
-  * each thread of execution: ingress, egress, and ghost.
-  */
+ * This class constructs the final placed MAU table control flow graph for
+ * each thread of execution: ingress, egress, and ghost.
+ */
 class BuildPowerGraph : public MauInspector, BFN::ControlFlowVisitor {
  public:
-  profile_t init_apply(const IR::Node *root) override;
-  bool preorder(const IR::MAU::TableSeq *seq) override;
-  bool preorder(const IR::MAU::Table *tbl) override;
-  /**
-    * Depending on compilation options, this function produces a dot graph
-    * for each thread of execution.  
-    */
-  void end_apply(const IR::Node *root) override;
+    profile_t init_apply(const IR::Node *root) override;
+    bool preorder(const IR::MAU::TableSeq *seq) override;
+    bool preorder(const IR::MAU::Table *tbl) override;
+    /**
+     * Depending on compilation options, this function produces a dot graph
+     * for each thread of execution.
+     */
+    void end_apply(const IR::Node *root) override;
 
-  BuildPowerGraph *clone() const override;
-  void flow_merge(Visitor &v) override;
+    BuildPowerGraph *clone() const override;
+    void flow_merge(Visitor &v) override;
 
-  SimplePowerGraph* get_graph(gress_t g);
-  BuildPowerGraph(const NextTable* next_table_properties,
-                  const BFN_Options& options);
-  BuildPowerGraph(const BuildPowerGraph& other) = default;
+    SimplePowerGraph *get_graph(gress_t g);
+    BuildPowerGraph(const NextTable *next_table_properties, const BFN_Options &options);
+    BuildPowerGraph(const BuildPowerGraph &other) = default;
 
  private:
-  SimplePowerGraph* ingress_graph_;
-  SimplePowerGraph* egress_graph_;
-  SimplePowerGraph* ghost_graph_;
+    SimplePowerGraph *ingress_graph_;
+    SimplePowerGraph *egress_graph_;
+    SimplePowerGraph *ghost_graph_;
 
  public:
-  const NextTable* next_table_properties_;
-  const BFN_Options& options_;
-  // Keep track of which logical tables are marked as always run.
-  std::vector<UniqueId> always_run_;
-  ordered_set<UniqueId> next_for(const IR::MAU::Table *tbl, cstring what) const;
+    const NextTable *next_table_properties_;
+    const BFN_Options &options_;
+    // Keep track of which logical tables are marked as always run.
+    std::vector<UniqueId> always_run_;
+    ordered_set<UniqueId> next_for(const IR::MAU::Table *tbl, cstring what) const;
 };
 
 };  // end namespace MauPower
