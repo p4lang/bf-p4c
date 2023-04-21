22f66bf5916eb0aac87cba64b62df352a613b839
Prathima Kotikalapudi <pkotikalapudi@barefootnetworks.com>
2023-04-21 10:47:47 -0700

    Update development environment (#7038)
    * Update development environment
    
    **Jenkins run**
    https://qa-jenkins-02-bxdsw.sc.intel.com/blue/organizations/jenkins/Nightly_dev-env_update/detail/Nightly_dev-env_update/859/pipeline
    
    **bf-p4c-compilers submodules**
     b1b763f9001863dfb843057b1359323b22e38b79 compiler_interfaces (b1b763f)
     cea63b889b3bfec444dc7dacc7a821539fda0e85 p4-tests/p4-programs (remotes/origin/master_poc-17-gcea63b88)
     76a26c668f0840c0700976e3d7b2c3da951de92b p4-tests/p4_14/switch (heads/master)
     4afb716948366025a76b31d6579a7a0a94c6145e p4-tests/p4_16/google-tor (remotes/origin/bf-tor)
    +b9c05f5a62aa0e8313e70c25217370884fb7e218 p4-tests/p4_16/switch_16 (heads/master)
     67d4abfcb21a9fc17c8362fe9b0bb8a52408c844 p4-tests/p4examples (heads/master)
     ca408b67e8c478f09506b774b51dd58348743965 p4-tests/p4testutils (remotes/origin/master_poc-1-gca408b6)
     1cbacf238296e21dbb9f6253f426e41723ed6be9 p4c (v1.2.3.8-7-g1cbacf238)
     de89c4fd016cf7d81562c4fbba9bfee4c3fceb35 third_party/spdlog (v1.2.1-1622-gde89c4fd)
    
    **Docker images**
    * Jarvis: amr-registry.caas.intel.com/bxd-sw/jarvis@sha256:494d351471102bb93282000d2e8a41a3e81f926406415944c84fcdad66923ab5
    * builder: amr-registry.caas.intel.com/bxd-sw/bf-p4c-builder@sha256:cef5cd8bfeaf62b3f76eaca70a8d37c53b8406b529f14a8f6cc68f642ee7d74a
    
    * Update Xfails.
    [no-jira]
    
    * Typo
    [no-jira]
    
    * We need both versions
    [no-jira]
    
    ---------
    
    Co-authored-by: BFN Jenkins Bot <jenkins@barefootnetworks.com>

--- p4-tests/JBayXfail.cmake
+++ p4-tests/JBayXfail.cmake
@@ -454,12 +454,16 @@ p4c_add_xfail_reason("tofino2"
   extensions/p4_tests/p4_16/compile_only/p4c-4498.p4
 )
 
-# TEMP FAILURE -- require model STF fix.  Once that is merged and docker image updated,
-# this failure should go away
-p4c_add_xfail_reason("tofino2"
-  "register rac128.*value is 0, expected 4822678189205111"
-  extensions/p4_tests/p4_16/stf/p4c-5192.p4
-)
+# dev env update 2023-04-20 (counter STF failures) 
+p4c_add_xfail_reason("tofino2"                
+   "bytes value is .*, expected .*"           
+   extensions/p4_tests/p4_14/stf/p4c-2911-2.p4
+)                                             
+
+p4c_add_xfail_reason("tofino2"                      
+   "bytes value is .*, expected .*" 
+   extensions/p4_tests/p4_16/stf/p4c-2911-2.p4     
+)                                                  
 
 # ALT-PHV: tests that do not work yet with the alternative allocator.
 # If you make an ALT-PHV test pass (or get close to it but if fails on later
